40dcea3c997f10b75166e7e599ae59df34d10843
KadTas <tas@simplon-charleville.fr>
2020-04-30 11:13:16 +0200

    un peu de poo

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php 
+     // on se connecte à notre base
+     $server = "db5000303655.hosting-data.io";
+     $dbname = "dbs296642";
+     $user = "dbu526627";
+     $pass = ")uq6PE.9";
+ 
+     try{
+        $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+        $bdd = new PDO("mysql:host=$server;dbname=$dbname", $user, $pass,$pdo_options);
+     }
+     catch(Exception $e){
+        die('Erreur : '.$e->getMessage());
+	}      
+ ?>
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -39,18 +54,20 @@
 					<div class="container">
 						<!-- logo -->
 						<div class="nav-logo">
-							<a href="index.html" class="logo"><img src="./img/logo.png" alt=""></a>
+							<a href="index.php" class="logo"><img src="./img/logo.png" alt=""></a>
 						</div>
 						<!-- /logo -->
 
 						<!-- nav -->
 						<ul class="nav-menu nav navbar-nav">
-							<li><a href="category.html">News</a></li>
-							<li><a href="category.html">Popular</a></li>
-							<li class="cat-1"><a href="category.html">Web Design</a></li>
-							<li class="cat-2"><a href="category.html">JavaScript</a></li>
-							<li class="cat-3"><a href="category.html">Css</a></li>
-							<li class="cat-4"><a href="category.html">Jquery</a></li>
+							<li><a href="login.php">Connexion</a></li>
+							<li><a href="register.php">Inscription</a></li>
+							<?php $req= $bdd->prepare("SELECT * FROM T_categories");
+							$req->execute();
+							//boucle pour tout afficher
+							while($donnees = $req->fetch()) { ?>
+						<li class="cat-<?php echo $donnees['categories_id']?>"><a href="category<?php echo $donnees['categories_id']?>.php"><?php echo $donnees['categories_name'] ?></a></li>
+							<?php } ?>
 						</ul>
 						<!-- /nav -->
 
@@ -73,7 +90,9 @@
 					<!-- nav -->
 					<div class="section-row">
 						<ul class="nav-aside-menu">
-							<li><a href="index.html">Home</a></li>
+							<li><a href="login.php">Connexion</a></li>
+							<li><a href="register.php">Inscription</a></li>
+							<li><a href="index.php">Home</a></li>
 							<li><a href="about.html">About Us</a></li>
 							<li><a href="#">Join Us</a></li>
 							<li><a href="#">Advertisement</a></li>
@@ -134,141 +153,36 @@
 		<div class="section">
 			<!-- container -->
 			<div class="container">
-				<!-- row -->
-				<div class="row">	
-					<!-- post -->
-					<div class="col-md-6">
-						<div class="post post-thumb">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-1.jpg" alt=""></a>
-							<div class="post-body">
-								<div class="post-meta">
-									<a class="post-category cat-2" href="category.html">JavaScript</a>
-									<span class="post-date">March 27, 2018</span>
-								</div>
-								<h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
-							</div>
-						</div>
-					</div>
-					<!-- /post -->
-
-					<!-- post -->
-					<div class="col-md-6">
-						<div class="post post-thumb">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-2.jpg" alt=""></a>
-							<div class="post-body">
-								<div class="post-meta">
-									<a class="post-category cat-3" href="category.html">Jquery</a>
-									<span class="post-date">March 27, 2018</span>
-								</div>
-								<h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
-							</div>
-						</div>
-					</div>
-					<!-- /post -->
-				</div>
-				<!-- /row -->
 
 				<!-- row -->
 				<div class="row">
 					<div class="col-md-12">
 						<div class="section-title">
-							<h2>Recent Posts</h2>
+							<h2>Articles Récents</h2>
 						</div>
 					</div>
 
 					<!-- post -->
+					<?php $req= $bdd->prepare("SELECT * FROM T_article, T_categories, R_lier WHERE T_article.article_id = R_lier.article_id AND R_lier.categories_id = T_categories.categories_id ORDER BY article_date DESC");
+							$req->execute();
+							//boucle pour tout afficher
+							while($donnees = $req->fetch()) { ?>
 					<div class="col-md-4">
 						<div class="post">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-3.jpg" alt=""></a>
+							<a class="post-img" href="blog-post.html"><img src="<?php echo $donnees['article_image']?>" alt=""></a>
 							<div class="post-body">
 								<div class="post-meta">
-									<a class="post-category cat-1" href="category.html">Web Design</a>
-									<span class="post-date">March 27, 2018</span>
+									<a class="post-category cat-<?php echo $donnees['categories_id']?>" href="category.html"><?php echo $donnees['categories_name'] ?></a>
+									<span class="post-date"><?php echo $donnees['article_date']?></span>
 								</div>
-								<h3 class="post-title"><a href="blog-post.html">Pagedraw UI Builder Turns Your Website Design Mockup Into Code Automatically</a></h3>
+								<h3 class="post-title"><a href="blog-post.html"><?php echo $donnees['article_title']?></a></h3>
 							</div>
 						</div>
 					</div>
-					<!-- /post -->
-
-					<!-- post -->
-					<div class="col-md-4">
-						<div class="post">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-4.jpg" alt=""></a>
-							<div class="post-body">
-								<div class="post-meta">
-									<a class="post-category cat-2" href="category.html">JavaScript</a>
-									<span class="post-date">March 27, 2018</span>
-								</div>
-								<h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
-							</div>
-						</div>
-					</div>
-					<!-- /post -->
-
-					<!-- post -->
-					<div class="col-md-4">
-						<div class="post">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-5.jpg" alt=""></a>
-							<div class="post-body">
-								<div class="post-meta">
-									<a class="post-category cat-3" href="category.html">Jquery</a>
-									<span class="post-date">March 27, 2018</span>
-								</div>
-								<h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
-							</div>
-						</div>
-					</div>
+							<?php } ?>
 					<!-- /post -->
 
 					<div class="clearfix visible-md visible-lg"></div>
-
-					<!-- post -->
-					<div class="col-md-4">
-						<div class="post">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-6.jpg" alt=""></a>
-							<div class="post-body">
-								<div class="post-meta">
-									<a class="post-category cat-2" href="category.html">JavaScript</a>
-									<span class="post-date">March 27, 2018</span>
-								</div>
-								<h3 class="post-title"><a href="blog-post.html">Why Node.js Is The Coolest Kid On The Backend Development Block!</a></h3>
-							</div>
-						</div>
-					</div>
-					<!-- /post -->
-
-					<!-- post -->
-					<div class="col-md-4">
-						<div class="post">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-1.jpg" alt=""></a>
-							<div class="post-body">
-								<div class="post-meta">
-									<a class="post-category cat-4" href="category.html">Css</a>
-									<span class="post-date">March 27, 2018</span>
-								</div>
-								<h3 class="post-title"><a href="blog-post.html">CSS Float: A Tutorial</a></h3>
-							</div>
-						</div>
-					</div>
-					<!-- /post -->
-
-					<!-- post -->
-					<div class="col-md-4">
-						<div class="post">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-2.jpg" alt=""></a>
-							<div class="post-body">
-								<div class="post-meta">
-									<a class="post-category cat-1" href="category.html">Web Design</a>
-									<span class="post-date">March 27, 2018</span>
-								</div>
-								<h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
-							</div>
-						</div>
-					</div>
-					<!-- /post -->
-				</div>
-				<!-- /row -->
 
 				<!-- row -->
 				<div class="row">
---
+++ poo/class_user.php
@@ -0,0 +1,58 @@
+<?php class User {
+    protected $_username;
+    protected $_password;
+    protected $_mail;
+    protected $_token;
+    protected $_validation;
+
+    public function __construct($_password, $_mail) {
+        $this->_password = $_password;
+        $this->_mail = $_mail;
+        $this->_token=substr(str_shuffle(str_repeat("0123456789azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN", 40)), 0, 40);
+    }
+
+    public function register($bdd) {
+        $req= $bdd->prepare('INSERT INTO T_user (user_login, user_mail, user_password, user_confirmation, user_token) VALUES (:user_login, :user_mail, :user_password, :user_confirmation, :user_token)');
+        $req->execute(array(
+        ':user_login' => $this->_username,
+        ':user_mail' => $this->_mail,
+        ':user_password' => $this->_password,
+        ':user_token' => $this->_token,
+        ':usertype_id' => 3,
+        ':user_confirmation' => 0));
+        return $req;
+    }
+
+    public function login($bdd) {
+        $req = $bdd->prepare("SELECT * FROM T_user WHERE user_mail = :email AND user_password = :pass");
+        $req->execute(array(
+            ':email' => $this->_mail,
+            ':pass' => $this->_password
+        ));
+
+        $count = $req->rowCount();
+        if($count > 0)
+    {
+    session_start();
+    $_SESSION['email'] = $this->_mail;
+    $_SESSION['pass'] = $this->_password;
+    header("location:../index.php?id=success");
+    }
+    else
+    {
+    //Mauvais identifiant ou mauvais tout cours
+    header("location:../login.php?id=fail");
+    }
+    }
+ 
+    public function sendmail($bdd) {
+        $req = $bdd->prepare("SELECT * FROM T_user WHERE user_mail = '$this->_mail'" );
+        $req->execute();
+        mail($this->_mail, "Test envoi de mail","Votre inscription a été effectuée, confirmez votre compte : http://tas.simplon-charleville.fr/poo-exo/confirm.php?id=$this->_token");
+    }
+
+    public function confirmed($bdd) {
+        $reqconfirm = $bdd->prepare("UPDATE T_user SET validate = 1 WHERE user_token = '$idtoken'");
+        $reqconfirm->execute();
+    }
+}
